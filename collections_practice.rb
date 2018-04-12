b91fe3d09a6969e0951d227d9e87a2633a2297c0
Anuradha Khambete <anukhambete@gmail.com>
2018-04-12 23:05:37 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -58,7 +58,10 @@ end
   
 def add_s(array)
   
-  array.each_with_index.collect {|element,index| "#{element}s" if index != 1}
-  
+  array.each_with_index.collect {|element,index| if index != 1
+  "#{element}s"
+  else
+  "#{element}"
+  }
 end
 
